8b7be0a4f3cc4b30548a4b3c28a7fe9415cd1ebb
Tom De Vree <tomdevree1980@gmail.com>
2020-03-28 17:16:04 +0100

    grpc service ingebouwd om data op te halen

---
+++ ClassManagement/Data/StudentService.cs
@@ -0,0 +1,25 @@
+﻿using ClassManagementService;
+using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassManagementClient.Data
+{
+    public class StudentService
+    {
+        private ClassManagementService.Student.StudentClient studentClient;
+        public StudentService()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            studentClient = new Student.StudentClient(channel);
+        }
+
+        public async Task<IEnumerable<ClassManagementService.StudentInClass>> ListStudentsForClass(string className)
+        {
+            ListStudentsForClassReply reply = await studentClient.ListStudentsForClassAsync(new ListStudentsForClassRequest { ClassName = className });
+            return reply.Students;
+        }
+    }
+}
---
+++ ClassManagementService/Services/StudentService.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassManagementService.Services
+{
+    public class StudentService : Student.StudentBase
+    {
+        public override Task<ListStudentsForClassReply> ListStudentsForClass(ListStudentsForClassRequest request, ServerCallContext context)
+        {
+            var reply = new ListStudentsForClassReply();
+            reply.Students.Add(new StudentInClass { StudentNumber = 1, FirstName = "Kobe", LastName = "De Vree", IsPresentToday = true });
+            reply.Students.Add(new StudentInClass { StudentNumber = 1, FirstName = "Yuna", LastName = "De Herdt", IsPresentToday = true });
+            return Task.FromResult(reply);
+        }
+    }
+}
